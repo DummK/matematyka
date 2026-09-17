fe6415dd224873d27740626e829f80c50572b7e1
filip <fpolski22@gmail.com>
2026-09-17 10:10:48 +0200

    Drugra iteracja poprawy wyswietlania pdfa, teraz jest calkiem dobrze

--- src/components/PdfViewer.tsx
+++ src/components/PdfViewer.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
@@ -7,17 +8,50 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 
 type PdfViewerProps = {
     fileName: string;
-}
-
+};
 
 function PdfViewer(props: PdfViewerProps) {
     const { fileName } = props;
 
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [containerWidth, setContainerWidth] = useState(0);
+
+    useEffect(() => {
+        const container = containerRef.current;
+
+        if (!container) {
+            return;
+        }
+
+        const observer = new ResizeObserver((entries) => {
+            const entry = entries[0];
+
+            setContainerWidth(entry.contentRect.width);
+        });
+
+        observer.observe(container);
+
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
     return (
-        <Document file={fileName}>
-            <Page pageNumber={1} />
-        </Document>
-    )
+        <div ref={containerRef} className="pdf-renderer">
+            <Document file={fileName}>
+                {
+                    containerWidth > 0 && (
+                        <Page
+                            pageNumber={1}
+                            width={containerWidth}
+                            renderTextLayer={false}
+                            renderAnnotationLayer={false}
+                        />
+                    )
+                }
+            </Document>
+        </div>
+    );
 }
 
 export default PdfViewer;
